126fcc2cfe5d375790d373d47575ad8972a3d236
jggon <jggon@DESKTOP-1U953OK>
2017-11-15 11:01:25 -0400

    Arreglé el constructor de Solicitud (agregué el atributo nuevo).

--- ProyectoFinal/src/logico/Solicitud.java
+++ ProyectoFinal/src/logico/Solicitud.java
@@ -3,7 +3,7 @@ package logico;
 import java.util.ArrayList;
 
 public class Solicitud {
-	
+
 	private String codigo;
 	private String fecha;
 	private Solicitante solicitante;
@@ -13,67 +13,83 @@ public class Solicitud {
 	private boolean mudarse;
 	private int experiencia;
 	private boolean activa;
-	
-	public Solicitud(String codigo, String fecha, Solicitante solicitante, boolean remoto, boolean licencia,
-			boolean mudarse, int experiencia) {
+
+	public Solicitud(String codigo, String fecha, Solicitante solicitante, ArrayList<String> idiomas, boolean remoto,
+			boolean licencia, boolean mudarse, int experiencia, boolean activa) {
 		super();
 		this.codigo = codigo;
 		this.fecha = fecha;
 		this.solicitante = solicitante;
-		this.idiomas = new ArrayList<>();
+		this.idiomas = idiomas;
 		this.remoto = remoto;
 		this.licencia = licencia;
 		this.mudarse = mudarse;
 		this.experiencia = experiencia;
+		this.activa = activa;
 	}
-	
+
 	public String getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getFecha() {
 		return fecha;
 	}
+
 	public void setFecha(String fecha) {
 		this.fecha = fecha;
 	}
+
 	public Solicitante getSolicitante() {
 		return solicitante;
 	}
+
 	public void setSolicitante(Solicitante solicitante) {
 		this.solicitante = solicitante;
 	}
+
 	public ArrayList<String> getIdiomas() {
 		return idiomas;
 	}
+
 	public void setIdiomas(ArrayList<String> idiomas) {
 		this.idiomas = idiomas;
 	}
+
 	public boolean isRemoto() {
 		return remoto;
 	}
+
 	public void setRemoto(boolean remoto) {
 		this.remoto = remoto;
 	}
+
 	public boolean isLicencia() {
 		return licencia;
 	}
+
 	public void setLicencia(boolean licencia) {
 		this.licencia = licencia;
 	}
+
 	public boolean isMudarse() {
 		return mudarse;
 	}
+
 	public void setMudarse(boolean mudarse) {
 		this.mudarse = mudarse;
 	}
+
 	public int getExperiencia() {
 		return experiencia;
 	}
+
 	public void setExperiencia(int experiencia) {
 		this.experiencia = experiencia;
 	}
-	
+
 }
