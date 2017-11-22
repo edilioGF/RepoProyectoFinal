db8f09bf1709f0e59bd83c35c850728ee998ec78
LuisIgnacio <LuisIgnacio@LuisIgnacio-Pc>
2017-11-22 11:25:15 -0400

    Creación de la vista registrar empresa. - LuisM

---
+++ ProyectoFinal/src/visual/RegEmpresa.java
@@ -0,0 +1,88 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Window.Type;
+import javax.swing.border.TitledBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+
+public class RegEmpresa extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JTextField textField_1;
+
+	
+	public RegEmpresa() {
+		setType(Type.UTILITY);
+		setTitle("Registro de Empresa");
+		setBounds(100, 100, 307, 309);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Informaci\u00F3n General", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(10, 11, 271, 215);
+		contentPanel.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblCdigo = new JLabel("C\u00F3digo:");
+		lblCdigo.setBounds(10, 24, 46, 14);
+		panel.add(lblCdigo);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 40, 110, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(10, 69, 61, 14);
+		panel.add(lblNombre);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(10, 85, 239, 23);
+		panel.add(textField_1);
+		
+		JLabel lblUbicacin = new JLabel("Ubicaci\u00F3n:");
+		lblUbicacin.setBounds(10, 114, 61, 14);
+		panel.add(lblUbicacin);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(10, 130, 239, 23);
+		panel.add(comboBox);
+		
+		JLabel lblTipo = new JLabel("Tipo:");
+		lblTipo.setBounds(10, 159, 46, 14);
+		panel.add(lblTipo);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(10, 175, 239, 23);
+		panel.add(comboBox_1);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("Siguiente");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancelar");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
