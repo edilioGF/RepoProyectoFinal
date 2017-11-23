9ab751980fc6a268f3fef8281fae352f4558e61a
20160197 <20160197@DESKTOP-ANGPPUV>
2017-11-23 19:29:07 -0400

    creacion de ventanas para los listados de match, empresa y solicitud.

---
+++ ProyectoFinal/src/visual/ListMatch.java
@@ -0,0 +1,56 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class ListMatch extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTable table;
+
+	
+	public ListMatch() {
+		setResizable(false);
+		setTitle("Listado de Matchs");
+		setBounds(100, 100, 700, 450);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JPanel panel = new JPanel();
+			panel.setBounds(10, 60, 664, 307);
+			contentPanel.add(panel);
+			panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+			{
+				JScrollPane scrollPane = new JScrollPane();
+				panel.add(scrollPane);
+				{
+					table = new JTable();
+					scrollPane.setViewportView(table);
+				}
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+		}
+	}
+
+}
