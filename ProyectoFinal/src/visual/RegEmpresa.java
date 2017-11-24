4b16e0f656001847e9000c57762e1f9f6100f413
LuisIgnacio <LuisIgnacio@LuisIgnacio-Pc>
2017-11-23 22:25:36 -0400

    Captura de datos para registrar empresa, y un metodito clean pal coro

--- ProyectoFinal/src/visual/RegEmpresa.java
+++ ProyectoFinal/src/visual/RegEmpresa.java
@@ -9,16 +9,24 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Window.Type;
 import javax.swing.border.TitledBorder;
+
+import logico.Controladora;
+import logico.Empresa;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class RegEmpresa extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField textField;
-	private JTextField textField_1;
-
+	private JTextField txtCodigo;
+	private JTextField txtNombre;
+	private JComboBox cbxUbicacion;
+	private JComboBox cbxTipo;
+	
 	public RegEmpresa() {
 		setType(Type.UTILITY);
 		setTitle("Registro de Empresa");
@@ -40,50 +48,74 @@ public class RegEmpresa extends JDialog {
 		lblCdigo.setBounds(10, 24, 46, 14);
 		panel.add(lblCdigo);
 
-		textField = new JTextField();
-		textField.setBounds(10, 40, 110, 20);
-		panel.add(textField);
-		textField.setColumns(10);
+		txtCodigo = new JTextField();
+		txtCodigo.setBounds(10, 40, 110, 20);
+		panel.add(txtCodigo);
+		txtCodigo.setColumns(10);
 
 		JLabel lblNombre = new JLabel("Nombre:");
 		lblNombre.setBounds(10, 69, 61, 14);
 		panel.add(lblNombre);
 
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(10, 85, 239, 23);
-		panel.add(textField_1);
+		txtNombre = new JTextField();
+		txtNombre.setColumns(10);
+		txtNombre.setBounds(10, 85, 239, 23);
+		panel.add(txtNombre);
 
 		JLabel lblUbicacin = new JLabel("Ubicaci\u00F3n:");
 		lblUbicacin.setBounds(10, 114, 61, 14);
 		panel.add(lblUbicacin);
 
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(10, 130, 239, 23);
-		panel.add(comboBox);
+		cbxUbicacion = new JComboBox();
+		cbxUbicacion.setBounds(10, 130, 239, 23);
+		panel.add(cbxUbicacion);
 
 		JLabel lblTipo = new JLabel("Tipo:");
 		lblTipo.setBounds(10, 159, 46, 14);
 		panel.add(lblTipo);
 
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(10, 175, 239, 23);
-		panel.add(comboBox_1);
+	    cbxTipo = new JComboBox();
+		cbxTipo.setBounds(10, 175, 239, 23);
+		panel.add(cbxTipo);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
 				JButton okButton = new JButton("Siguiente");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						String codigo = txtCodigo.getText();
+						String nombre = txtNombre.getText();
+						String ubicacion = cbxUbicacion.getSelectedItem().toString();
+						String tipo = cbxTipo.getSelectedItem().toString();
+						Empresa empresa = new Empresa(codigo , nombre , ubicacion , tipo);
+						Controladora.getInstance().addEmpresa(empresa);
+						clean();
+					}
+				});
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
 			}
 			{
 				JButton cancelButton = new JButton("Cancelar");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
 		}
+	}
+	private void clean() {
+		txtNombre.setText("");
+		txtCodigo.setText("");
+		cbxUbicacion.setSelectedIndex(-1);
+		cbxTipo.setSelectedIndex(-1);
+		;
+		
 	}
 }
