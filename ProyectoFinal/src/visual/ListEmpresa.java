cc6fd515cb501222b77b3317810bc88cfe8ba758
JG0328 <jgg032899@gmail.com>
2017-11-25 09:26:34 -0400

    Se listan las empresas y se pueden filtrar por tipo

--- ProyectoFinal/src/visual/ListEmpresa.java
+++ ProyectoFinal/src/visual/ListEmpresa.java
@@ -19,6 +19,10 @@ import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.border.EtchedBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class ListEmpresa extends JDialog {
 
@@ -26,6 +30,8 @@ public class ListEmpresa extends JDialog {
 	private JTable table;
 	private Object[] fila;
 	private DefaultTableModel model;
+
+	private JComboBox cbxTipo;
 
 	public ListEmpresa() {
 		setResizable(false);
@@ -38,7 +44,7 @@ public class ListEmpresa extends JDialog {
 		contentPanel.setLayout(null);
 
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 60, 664, 307);
+		panel.setBounds(12, 49, 678, 350);
 		contentPanel.add(panel);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
 
@@ -52,11 +58,25 @@ public class ListEmpresa extends JDialog {
 		String[] columns = { "Código", "Nombre", "Ubicación", "Tipo" };
 		model = new DefaultTableModel();
 		model.setColumnIdentifiers(columns);
-		loadTable();
 
 		scrollPane.setViewportView(table);
+
+		JLabel lblTipo = new JLabel("Tipo:");
+		lblTipo.setBounds(12, 22, 50, 15);
+		contentPanel.add(lblTipo);
+
+		cbxTipo = new JComboBox();
+		cbxTipo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				loadTable(cbxTipo.getSelectedItem().toString());
+			}
+		});
+		cbxTipo.setModel(new DefaultComboBoxModel(new String[] { "<Todos>" }));
+		cbxTipo.setBounds(80, 17, 250, 23);
+		contentPanel.add(cbxTipo);
 		{
 			JPanel buttonPane = new JPanel();
+			buttonPane.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 
@@ -68,20 +88,31 @@ public class ListEmpresa extends JDialog {
 			});
 			buttonPane.add(btnCerrar);
 		}
+
+		load();
+		loadTable(cbxTipo.getSelectedItem().toString());
 	}
 
-	private void loadTable() {
+	private void load() {
+		for (String tipo : Controladora.getMisTiposDeEmpresa()) {
+			cbxTipo.addItem(tipo);
+		}
+	}
+
+	private void loadTable(String tipo) {
 		// TODO Auto-generated method stub
 		model.setRowCount(0);
 		fila = new Object[model.getColumnCount()];
 
 		for (Empresa empresa : Controladora.getInstance().getMisEmpresas()) {
-			fila[0] = empresa.getCodigo();
-			fila[1] = empresa.getNombre();
-			fila[2] = empresa.getUbicacion();
-			fila[3] = empresa.getTipo();
+			if (tipo.equalsIgnoreCase("<Todos>") || tipo.equalsIgnoreCase(empresa.getTipo())) {
+				fila[0] = empresa.getCodigo();
+				fila[1] = empresa.getNombre();
+				fila[2] = empresa.getUbicacion();
+				fila[3] = empresa.getTipo();
 
-			model.addRow(fila);
+				model.addRow(fila);
+			}
 		}
 
 		table.setModel(model);
