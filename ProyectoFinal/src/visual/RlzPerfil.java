561a043da9a991c3aec4700e68a1c48a815ebaa6
20160197 <20160197@DESKTOP-ANGPPUV>
2017-11-27 19:47:34 -0400

    agregue mas campos de informacion a RlzPerfil

--- ProyectoFinal/src/visual/RlzPerfil.java
+++ ProyectoFinal/src/visual/RlzPerfil.java
@@ -19,6 +19,8 @@ import javax.swing.UIManager;
 import java.awt.Color;
 import javax.swing.JRadioButton;
 import javax.swing.JComboBox;
+import javax.swing.JSpinner;
+import javax.swing.JCheckBox;
 
 public class RlzPerfil extends JDialog {
 
@@ -35,12 +37,17 @@ public class RlzPerfil extends JDialog {
 	private JPanel pnlTecnico;
 	private JPanel pnlObrero;
 	private JPanel pnlGraduado;
+	private JTextField txtFecha;
+	private JComboBox cbxIdioma;
+	private JSpinner spnExp;
+	private JCheckBox cbMudarse;
+	private JCheckBox cbLicencia;
 
 	public RlzPerfil() {
 		setTitle("Realizar Perfil");
 		setModal(true);
 		setResizable(false);
-		setBounds(100, 100, 390, 395);
+		setBounds(100, 100, 390, 614);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -49,7 +56,7 @@ public class RlzPerfil extends JDialog {
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(null, "Informacion del Solicitante", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel.setBounds(10, 11, 364, 157);
+		panel.setBounds(10, 11, 364, 151);
 		contentPanel.add(panel);
 		panel.setLayout(null);
 		
@@ -67,11 +74,11 @@ public class RlzPerfil extends JDialog {
 		panel.add(btnNewButton);
 		
 		JLabel lblNombre = new JLabel("Nombre:");
-		lblNombre.setBounds(10, 69, 59, 14);
+		lblNombre.setBounds(10, 65, 59, 14);
 		panel.add(lblNombre);
 		
 		txtNombre = new JTextField();
-		txtNombre.setBounds(60, 65, 284, 23);
+		txtNombre.setBounds(60, 62, 284, 23);
 		panel.add(txtNombre);
 		txtNombre.setColumns(10);
 		
@@ -88,7 +95,7 @@ public class RlzPerfil extends JDialog {
 		panel_1.setLayout(null);
 		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Formaci\u00F3n",
 						TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel_1.setBounds(10, 175, 364, 70);
+		panel_1.setBounds(10, 394, 364, 70);
 		contentPanel.add(panel_1);
 		
 		rdbtnGraduado = new JRadioButton("Graduado");
@@ -98,7 +105,7 @@ public class RlzPerfil extends JDialog {
 			}
 		});
 		rdbtnGraduado.setSelected(true);
-		rdbtnGraduado.setBounds(9, 23, 109, 23);
+		rdbtnGraduado.setBounds(6, 23, 109, 23);
 		panel_1.add(rdbtnGraduado);
 		
 		rdbtnTecnico = new JRadioButton("T\u00E9cnico");
@@ -123,7 +130,7 @@ public class RlzPerfil extends JDialog {
 		
 		pnlTecnico = new JPanel();
 		pnlTecnico.setBorder(new TitledBorder(null, "T\u00E9cnico", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		pnlTecnico.setBounds(10, 253, 364, 70);
+		pnlTecnico.setBounds(10, 475, 364, 70);
 		contentPanel.add(pnlTecnico);
 		pnlTecnico.setLayout(null);
 		
@@ -137,7 +144,7 @@ public class RlzPerfil extends JDialog {
 		
 		pnlObrero = new JPanel();
 		pnlObrero.setBorder(new TitledBorder(null, "Obrero", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		pnlObrero.setBounds(10, 253, 364, 70);
+		pnlObrero.setBounds(10, 475, 364, 70);
 		contentPanel.add(pnlObrero);
 		pnlObrero.setLayout(null);
 		
@@ -151,7 +158,7 @@ public class RlzPerfil extends JDialog {
 		
 		JPanel pnlGraduado = new JPanel();
 		pnlGraduado.setBorder(new TitledBorder(null, "Graduado", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		pnlGraduado.setBounds(10, 253, 364, 70);
+		pnlGraduado.setBounds(10, 475, 364, 70);
 		contentPanel.add(pnlGraduado);
 		pnlGraduado.setLayout(null);
 		
@@ -162,6 +169,59 @@ public class RlzPerfil extends JDialog {
 		cbxAestudio = new JComboBox();
 		cbxAestudio.setBounds(108, 27, 230, 20);
 		pnlGraduado.add(cbxAestudio);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setLayout(null);
+		panel_2.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informaci\u00F3n Laboral", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_2.setBounds(10, 173, 356, 210);
+		contentPanel.add(panel_2);
+		
+		JLabel label_1 = new JLabel("Fecha:");
+		label_1.setBounds(10, 21, 53, 14);
+		panel_2.add(label_1);
+		
+		JLabel label_2 = new JLabel("Idioma:");
+		label_2.setBounds(10, 70, 59, 14);
+		panel_2.add(label_2);
+		
+		JLabel label_3 = new JLabel("A\u00F1os de experiencia:");
+		label_3.setBounds(195, 70, 128, 14);
+		panel_2.add(label_3);
+		
+		JLabel label_4 = new JLabel("Licencia para conducir");
+		label_4.setBounds(10, 140, 135, 14);
+		panel_2.add(label_4);
+		
+		txtFecha = new JTextField();
+		txtFecha.setText("27/11/2017 19:28");
+		txtFecha.setEditable(false);
+		txtFecha.setColumns(10);
+		txtFecha.setBounds(10, 36, 151, 23);
+		panel_2.add(txtFecha);
+		
+		cbxIdioma = new JComboBox();
+		cbxIdioma.setBounds(10, 90, 151, 23);
+		panel_2.add(cbxIdioma);
+		
+		spnExp = new JSpinner();
+		spnExp.setBounds(195, 90, 105, 23);
+		panel_2.add(spnExp);
+		
+		cbLicencia = new JCheckBox("");
+		cbLicencia.setBounds(169, 136, 20, 23);
+		panel_2.add(cbLicencia);
+		
+		JLabel label_5 = new JLabel("Disposici\u00F3n a mudarse");
+		label_5.setBounds(10, 170, 135, 14);
+		panel_2.add(label_5);
+		
+		cbMudarse = new JCheckBox("");
+		cbMudarse.setBounds(169, 166, 20, 23);
+		panel_2.add(cbMudarse);
+		
+		JLabel label_6 = new JLabel("A\u00F1os");
+		label_6.setBounds(304, 95, 42, 14);
+		panel_2.add(label_6);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
